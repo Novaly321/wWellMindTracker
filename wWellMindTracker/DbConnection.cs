2a83e5eff4a9e4f32db26bb30fe389941040e4f5
Juan Esteban <Juan Esteban@LAPTOP-ORO9EF7S>
2025-11-20 21:52:41 -0500

    feat: implementar clase DbConnection usando patrón Singleton para la conexión a SQL Server
    - Se creó la clase DbConnection
    - Implementa el patrón Singleton garantizando una sola instancia de conexión
    - Centraliza la cadena de conexión para toda la aplicación
    - Permite que los repositorios accedan a una única conexión compartida
    - Prepara la base para integración con los repositorios de Usuario, Emoción y RegistroEmocion

---
+++ wWellMindTracker/DbConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace wWellMindTracker.Data
+{
+    public class DbConnection
+    {
+
+        private static SqlConnection _connection;
+
+        private DbConnection() { }
+
+        public static SqlConnection GetConnection()
+        {
+
+            if (_connection == null)
+            {
+
+                _connection = new SqlConnection("Server=.;Database=WellMindTracker;Trusted_Connection=True;");
+
+            }
+
+            return _connection;
+        }
+
+    }
+}
